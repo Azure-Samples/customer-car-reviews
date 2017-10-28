1681408ac2c4a732e09ba1884e2e618d7724c3af
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-10-28 15:16:42 +0900

    Feature: Confirm Dialog

--- CarReviews/car-reviews/src/app/material.module.ts
+++ CarReviews/car-reviews/src/app/material.module.ts
@@ -8,7 +8,8 @@ import {
   MatCardModule,
   MatGridListModule,
   MatButtonModule,
-  MatInputModule
+  MatInputModule,
+  MatDialogModule,
 } from '@angular/material';
 
 @NgModule({
@@ -20,7 +21,8 @@ import {
     MatCardModule,
     MatGridListModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatDialogModule
   ],
   exports: [
     MatMenuModule,
@@ -30,7 +32,8 @@ import {
     MatCardModule,
     MatGridListModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatDialogModule
   ]
 })
 export class MaterialModule {}
