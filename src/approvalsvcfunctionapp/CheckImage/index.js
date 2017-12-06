b4bf38e56c77b4f0415d482da59d3cf023ee8f71
Thiago Almeida <thalme@microsoft.com>
2017-12-05 22:03:00 -0200

    Trying request-retry

--- src/approvalsvcfunctionapp/CheckImage/index.js
+++ src/approvalsvcfunctionapp/CheckImage/index.js
@@ -1,55 +1,55 @@
-var request = require('request-promise');
+var request = require('requestretry');
 
 module.exports = function (context, carReviewTextChecked) {
 
-if(context.bindingData.dequeueCount > 2) {
-  context.bindings.poisonedMessage = carReviewTextChecked;
-  context.done();
-}
+    if(context.bindingData.dequeueCount > 2) {
+    context.bindings.poisonedMessage = carReviewTextChecked;
+    context.done();
+    }
 
-if (carReviewTextChecked && (!carReviewTextChecked.imageApproval ||  carReviewTextChecked.imageApproval  != "complete")) {
+    if (carReviewTextChecked && (!carReviewTextChecked.imageApproval ||  carReviewTextChecked.imageApproval  != "complete")) {
 
-    var options = {
-        uri: process.env["VisionApiUrl"],
-        method: 'POST',
-        qs: {
-            visualFeatures: 'Description'
-        },
-        headers: {
-            'Ocp-Apim-Subscription-Key': process.env["VisionApiKey"],
-            'Content-Type': 'application/json'
-        },
-        body :{ "url" : carReviewTextChecked.image_url },
-        json: true
-    };
+        var options = {
+            uri: process.env["VisionApiUrl"],
+            method: 'POST',
+            qs: {
+                visualFeatures: 'Description'
+            },
+            headers: {
+                'Ocp-Apim-Subscription-Key': process.env["VisionApiKey"],
+                'Content-Type': 'application/json'
+            },
+            body :{ "url" : carReviewTextChecked.image_url },
+            json: true
+        };
 
-    request(options)
-    .then(function (parsedBody) {
-        var tags = parsedBody.description.tags;
-        carReviewTextChecked.imageApproval = "complete";
-        if(tags.indexOf("car") > -1){
-            carReviewTextChecked.state = "approved";
-            context.bindings.outputDocument = carReviewTextChecked;
-        } else {
-            carReviewTextChecked.state = "rejected";
-            context.bindings.outputDocument = carReviewTextChecked;
-            var rejectionEvent = {
-                id: carReviewTextChecked.id,
-                company: carReviewTextChecked.company,
-                description: carReviewTextChecked.description,
-                image_url: carReviewTextChecked.image_url,
-                name: carReviewTextChecked.name,
-                state: carReviewTextChecked.state,
-                rejectionReason: "car is not on the image"
-            };
-            context.bindings.rejectedReviewEvent = rejectionEvent;
-        }
-        context.done();
-    })
-    .catch(function (err) {
-       context.log(err);
-       context.done(err, {});
-    });
+        request(options)
+        .then(function (parsedBody) {
+            var tags = parsedBody.description.tags;
+            carReviewTextChecked.imageApproval = "complete";
+            if(tags.indexOf("car") > -1){
+                carReviewTextChecked.state = "approved";
+                context.bindings.outputDocument = carReviewTextChecked;
+            } else {
+                carReviewTextChecked.state = "rejected";
+                context.bindings.outputDocument = carReviewTextChecked;
+                var rejectionEvent = {
+                    id: carReviewTextChecked.id,
+                    company: carReviewTextChecked.company,
+                    description: carReviewTextChecked.description,
+                    image_url: carReviewTextChecked.image_url,
+                    name: carReviewTextChecked.name,
+                    state: carReviewTextChecked.state,
+                    rejectionReason: "car is not on the image"
+                };
+                context.bindings.rejectedReviewEvent = rejectionEvent;
+            }
+            context.done();
+        })
+        .catch(function (err) {
+            context.log(err);
+            context.done(err, {});
+        });
     }
     else {
 
