abecafd79881f0c365edbc94dde9bdd040d68ff5
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-12-10 10:30:48 -0800

    fix slow FileUpload
    It might be caused  unncessesary npm packages.
    I'm in airport so I can't test it, however, could you try this one and if it ok, please merge this.
    
    When I investigate the code, no npm install needed for this code.

---
+++ CarReviews/car-reviews/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  fileUploadUrl: "https://carreviewbackend.azurewebsites.net/api/FileUploadNode/{filename}?code=ELGpkEw8rewQJV/pQPZgJ6J7zyIe2DSjlS7iy7TrdCb0ymH23HDdIQ==",
+  getCarsUrl: "https://carreviewbackend.azurewebsites.net/api/GetCars/{state}?code=1kv1BxaMYB/0aSvEagMDYzixfEFMkehTCkfgZQGLDYf5FHnF4x2Iow==",
+  createCarUrl: "https://prod-04.japaneast.logic.azure.com:443/workflows/927454cc9b04458cb57f64fec0508327/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=tG6b9ETTbD-Gee2ZPoHb6PSq5m0qHG1wxW5PjxubSL0",
+  imageBlobUrl: "https://carreviewstorage.blob.core.windows.net/outcontainer/",
+};
